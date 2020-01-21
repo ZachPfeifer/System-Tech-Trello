ec6fd4570ae0305e9057d8eb7df2e9200c1d4546
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-21 12:00:57 -0700

    tiny styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,21 +3,24 @@ import Task from './Task'
 
 export default function Board({ board, boardIndex, moveLeft, moveRight, addTask, removeTask }) {
   return (
-    <div className="col">
-      <h1>{board.name}</h1>
-      {board.tasks.map((task, taskIndex) =>
-        <Task
-          task={task}
-          taskIndex={taskIndex}
-          canMoveLeft={boardIndex !== 0}
-          canMoveRight={boardIndex !== 3}
-          key={taskIndex}
-          moveLeft={() => moveLeft(taskIndex)}
-          moveRight={() => moveRight(taskIndex)}
-          removeTask={removeTask}
-        />
-      )}
-      <button onClick={addTask}>+</button>
+    <div className="col-3">
+      <div className="text-white text-center bg-dark mb-3">
+
+        <h1 className="card-title">{board.name}</h1>
+        {board.tasks.map((task, taskIndex) =>
+          <Task
+            task={task}
+            taskIndex={taskIndex}
+            canMoveLeft={boardIndex !== 0}
+            canMoveRight={boardIndex !== 3}
+            key={taskIndex}
+            moveLeft={() => moveLeft(taskIndex)}
+            moveRight={() => moveRight(taskIndex)}
+            removeTask={removeTask}
+          />
+        )}
+        <button onClick={addTask}>+</button>
+      </div>
     </div>
   )
 }

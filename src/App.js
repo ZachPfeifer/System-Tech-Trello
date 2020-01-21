1a53e3380c07b4626ebe75e9c5cba52f17f31caf
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-21 11:27:18 -0700

    App.js layout done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,79 @@
 import React, { Component } from 'react'
+import Board from './components/Board'
 
 export default class App extends Component {
+  //SECTION STATE 
   constructor(props) {
     super(props)
     this.state = {
 
-
+      boards: [
+        {
+          name: "Would be Nice",
+          tasks: [
+            {
+              name: "Task A"
+              //IUUD MAYBE
+            }
+          ]
+        },
+        {
+          name: "To Do",
+          tasks: [
+            {
+              name: "Task B"
+              //IUUD MAYBE
+            }
+          ]
+        },
+        {
+          name: "In Progress",
+          tasks: [
+            {
+              name: "Task C"
+              //IUUD MAYBE
+            }
+          ]
+        },
+        {
+          name: "Completed",
+          tasks: [
+            {
+              name: "Task D"
+              //IUUD MAYBE
+            }
+          ]
+        },
+      ]
     }
   }
+
+  //SECTION HANDLES
+  handleAdd() {
+
+  }
+  handleDelete() {
+
+  }
+  handleMove() {
+
+  }
+
+  //SECTION APP
   render() {
     return (
-      <div>
-
+      <div className="App">
+        {this.state.columns.map((board, boardIndex) => (
+          <Board
+            board={board}
+            boardIndex={boardIndex}
+            key={boardIndex}
+          // movetask={}
+          // addTask={}
+          // removeTask={}
+          />
+        )
+        )}
       </div>
     )
   }

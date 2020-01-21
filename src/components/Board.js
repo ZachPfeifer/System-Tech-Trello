f5067556332cd256c23543fd35114f2231b77947
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-21 11:32:05 -0700

    Board Layout Done

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Task from './Task'
 
-export default function Board() {
+export default function Board(
+  board,
+  boardIndex,
+  // moveTask
+  // addTask
+  // removeTask
+) {
   return (
-    <div>
-
+    <div className="col-3">
+      <h1>{board.name}</h1>
+      {board.tasks.map((task, taskIndex) =>
+        <Task
+          task={task}
+          taskIndex={taskIndex}
+          canMoveLeft={boardIndex !== 0}
+          canMoveRight={boardIndex !== 3}
+          key={taskIndex}
+        // removeTask={removeTask}
+        />
+      )}
+      {/* <button onClick={addTask} className="btn btn-primary">+</button> */}
     </div>
   )
 }
